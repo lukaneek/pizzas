6a841483f80df5a61ebe7d352e4aaf9bef097585
Luka <lukaneek@gmail.com>
2025-03-14 12:03:14 -0500

    update

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -5,6 +5,5 @@ mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
-
     .then(() => console.log(`Connected to ${dbName} database!`))
     .catch((err) => console.log(err)); 
