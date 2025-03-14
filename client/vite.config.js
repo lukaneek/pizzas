a6e4213ae7f3d968af768aa2ba7b5254f9e8a3be
Luka <lukaneek@gmail.com>
2025-03-14 11:31:06 -0500

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   base: "./",
   plugins: [react()],
   preview: {
-    port: 3000,
+    port: 8080,
     strictPort: true,
   },
   server: {
-    port: 3000,
+    port: 8080,
     strictPort: true,
     host: true,
     origin: "http://0.0.0.0:3000",
