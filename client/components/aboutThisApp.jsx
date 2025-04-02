380b7895a0231cf0657683aaeb4ca35f02bfcafb
Luka <lukaneek@gmail.com>
2025-04-02 09:57:01 -0500

    update

--- client/components/aboutThisApp.jsx
+++ client/components/aboutThisApp.jsx
@@ -5,6 +5,7 @@ const AboutThisApp = () => {
             <h3>About this app</h3>
             <p>This is a JavaScript app that uses a CI/CD pipeline through github, deployed on two EC2 instances in AWS.  The first instance hosts the React frontend.  The second instance runs an Express server, providing the API's that are called from the React frontend. Any changes committed to this github repository auto updates this app.</p>
             <p/>
+            <p>This application will allow the user to order pizzas.  The app requires registration and email verification to create an account.</p>
             <a target="_blank" href="https://github.com/lukaneek/pizzas">Link To This Applications Repository</a>
         </>
     );
